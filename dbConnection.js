a8b29968bb7097a5a9c6933568c6bffe2432a616
unknown <saurabh.parihar@SAURABH-PARIHAR.in.corp.tavant.com>
2016-08-29 16:54:26 +0530

    added RedHat Environment variables for DB

--- dbConnection.js
+++ dbConnection.js
@@ -1,17 +1,13 @@
 module.exports = {
 
+	mysql: undefined,
 	con: undefined,
 	connection: undefined,
+	dbConfig: undefined,
 
 	createConnection: function() {
-		var mysql = require("mysql");
-		this.con = mysql.createConnection({
-		  host: "localhost",
-		  user: "root",
-		  password: "root",
-		  database: "xnode"
-		});
-		this.connection=this.con;
+		this.init();
+		this.con = this.mysql.createConnection(this.dbConfig);
 	},	
 
 	connect: function() {
@@ -31,5 +27,15 @@ module.exports = {
 		// Ensures all previously enqueued queries are still
 		// before sending a COM_QUIT packet to the MySQL server.
 		});
+	},
+
+	init: function() {
+		this.mysql = require("mysql");
+		this.dbConfig={};
+		this.dbConfig.host = process.env.OPENSHIFT_MYSQL_DB_HOST || "localhost";
+		this.dbConfig.user = process.env.OPENSHIFT_MYSQL_DB_PORT || "root";
+		this.dbConfig.password = process.env.OPENSHIFT_MYSQL_DB_USERNAME || "root";
+		this.dbConfig.database = "xnode";		
+		this.connection=this.con; // just an alias
 	}
 }
